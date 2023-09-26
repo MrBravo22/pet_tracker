9002a6566c321f3d4209f6facbe9f89a6f5dc920
Elijah Bravo <elijahbravo22@yahoo.com>
2023-09-25 22:56:14 -0600

    seeded data

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const client = new pg.Client('postgres://localhost/pet_tracker_db');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -20,8 +20,29 @@ const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
+  DROP TABLE IF EXISTS pets;
+  DROP TABLE IF EXISTS users;
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100)
+    );
+    CREATE TABLE pets(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100),
+      user_id INTEGER REFERENCES users(id)
+    );
+    INSERT INTO users(name) VALUES ('Eli');
+    INSERT INTO users(name) VALUES ('Kelsey');
+    INSERT INTO users(name) VALUES ('Charlie');
+    INSERT INTO users(name) VALUES ('Luna');
+    INSERT INTO pets(name, user_id) VALUES ('Bowser', (SELECT id FROM users WHERE name='Eli'));
+    INSERT INTO pets(name, user_id) VALUES ('Jamal', (SELECT id FROM users WHERE name='Eli'));
+    INSERT INTO pets(name) VALUES ('Onyx');
+    INSERT INTO pets(name) VALUES ('Canyon');
+    INSERT INTO pets(name) VALUES ('Lightning');
+    INSERT INTO pets(name) VALUES ('Thunder');
   `;
+  await client.query(SQL);
   console.log('create your tables and seed data');
 
   const port = process.env.PORT || 3000;

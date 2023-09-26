0d3cc9dc2b2133eaef97cf332b758028d62aee4e
Elijah Bravo <elijahbravo22@yahoo.com>
2023-09-26 08:23:12 -0600

    building UI

--- src/index.js
+++ src/index.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
 const App = ()=> {
+  const [users, setUsers] = useState([]);
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await axios.get('/api/users')
+      setUsers(response.data);
+    }
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      const response = await axios.get('/api/pets')
+      setPets(response.data);
+    }
+    fetchPets();
+  }, []);
+
   return (
-    <h1>FullStack Template</h1>
+    <div>
+      <h1>Pet Tracker</h1>
+      <main>
+        <div>
+          <h2>Users ({ users.length })</h2>
+          <ul>
+            {
+              users.map( user => {
+                const usersPets = pets.filter(pet => pet_user_id === user.id);
+                return (
+                  <li key={ user.id }>
+                    { user.name }
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+        <div>
+          <h2>Pets ({ pets.length })</h2>
+          <ul>
+            {
+              pets.map( pet => {
+                return (
+                  <li key={ pet.id }>
+                    { pet.name }
+                  </li>
+                );
+              })
+            }
+            </ul>
+        </div>
+      </main>
+    </div>
   );
 };
 
